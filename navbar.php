703c2850e2029a5d82f64ac212445d723c2bcc7e
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2023-12-25 15:55:42 +0800

    adding sidebar

--- navbar.php
+++ navbar.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Responsive sidebar template with sliding effect and dropdown menu based on bootstrap 3">
     <title>Sidebar template</title>
+    <link rel="stylesheet" href="Styles/css.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 
@@ -23,7 +24,7 @@
         <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
             <i class="fas fa-bars"></i>
         </a>
-        <nav id="sidebar" class="sidebar-wrapper">
+        <nav id="sidebar" class="sidebar-wrapper bg-black">
             <div class="sidebar-content">
                 <div class="sidebar-brand">
                     <a href="#">pro sidebar</a>
@@ -33,7 +34,7 @@
                 </div>
                 <div class="sidebar-header">
                     <div class="user-pic">
-                        <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg" alt="User picture">
+                        <!-- <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg" alt="User picture"> -->
                     </div>
                     <div class="user-info">
                         <span class="user-name">Jhon
@@ -201,19 +202,19 @@
             <!-- sidebar-content  -->
             <div class="sidebar-footer">
                 <a href="#">
-                    <i class="fa fa-bell"></i>
+                    <i class="fa fa-bell mt-2"></i>
                     <span class="badge badge-pill badge-warning notification">3</span>
                 </a>
                 <a href="#">
-                    <i class="fa fa-envelope"></i>
+                    <i class="fa fa-envelope mt-2"></i>
                     <span class="badge badge-pill badge-success notification">7</span>
                 </a>
                 <a href="#">
-                    <i class="fa fa-cog"></i>
-                    <span class="badge-sonar"></span>
+                    <i class="fa fa-cog mt-2"></i>
+                    <span class="badge-sonar mt-1"></span>
                 </a>
                 <a href="#">
-                    <i class="fa fa-power-off"></i>
+                    <i class="fa fa-power-off mt-2"></i>
                 </a>
             </div>
         </nav>
@@ -295,6 +296,7 @@
         <!-- page-content" -->
     </div>
     <!-- page-wrapper -->
+    <script src="Js/js.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
