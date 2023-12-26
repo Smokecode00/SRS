d2e522aaec2b91df59e488e8f819f0c1c62da60a
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2023-12-26 21:59:29 +0800

    add

--- dashboard.php
+++ dashboard.php
@@ -146,6 +146,30 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-12 col-md-6 d-flex">
+                            <div class="card flex-fill border-0">
+                                <div class="card-body py-4">
+                                    <div class="d-flex align-items-start">
+                                        <div class="flex-grow-1">
+                                            <h4 class="mb-2">
+                                                $78.00
+                                            </h4>
+                                            <p class="mb-2">
+                                                Total Earnings
+                                            </p>
+                                            <div class="mb-0">
+                                                <span class="badge text-success me-2">
+                                                    +9.0%
+                                                </span>
+                                                <span class="text-muted">
+                                                    Since Last Month
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
